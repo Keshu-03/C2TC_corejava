8f24b55f97c1e000eba95e4f0b74f8cb684fcf7c
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-21 16:25:18 +0530

    Addition

---
+++ C2C_KESHU/src/DAY1/encapsulation/StudentDemo.java
@@ -0,0 +1,19 @@
+package DAY1.encapsulation;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj=new Student();
+		obj.setId(101);
+		obj.setName("Keshu");
+		System.out.println(obj.getId());
+		System.out.println(obj.getName());
+		System.out.println();
+		System.out.println(obj);
+		
+		
+
+	}
+
+}
