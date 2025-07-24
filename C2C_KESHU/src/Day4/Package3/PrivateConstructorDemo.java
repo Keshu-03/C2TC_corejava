a13236a1773f59aefd0d07584938f352e1543e4f
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-24 20:35:37 +0530

    Third Package

---
+++ C2C_KESHU/src/Day4/Package3/PrivateConstructorDemo.java
@@ -0,0 +1,23 @@
+package Day4.Package3;
+
+public class PrivateConstructorDemo {
+
+	//Program to demonstrate Private Constructor and Singleton class
+	
+		public static void main(String s[]) {
+			// Can't create object with private constructor
+			// MyClass m1=new MyClass();
+			
+			MyClass m=MyClass.getObject();
+			m.setId(10);
+			
+			MyClass m1=MyClass.getObject();
+			
+			System.out.println(m);
+			System.out.println(m1);
+			
+		}
+	}
+
+
+
