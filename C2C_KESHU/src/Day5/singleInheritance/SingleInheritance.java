ebd53558f93bb927121521a4e55878a507cb43b6
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-24 21:05:56 +0530

    Single Inheritance

---
+++ C2C_KESHU/src/Day5/singleInheritance/SingleInheritance.java
@@ -0,0 +1,26 @@
+package Day5.singleInheritance;
+
+
+//Program to demonstrate Single Inheritance 
+
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		
+		citizen c = new citizen("Partha", "258741962525", "Chennai", 9159186845L);
+		System.out.println(c);
+		
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+}
+
+	
+
+	
+
+
+
+
+
