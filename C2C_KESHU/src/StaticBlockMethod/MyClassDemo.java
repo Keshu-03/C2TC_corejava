06f68daee4bea67cf83a65caf0af0b406e2f74f4
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-27 19:03:02 +0530

    StaticBlockMethod

---
+++ C2C_KESHU/src/StaticBlockMethod/MyClassDemo.java
@@ -0,0 +1,25 @@
+package StaticBlockMethod;
+
+public class MyClassDemo {//Program to demonstrate static method
+	
+
+		public static void main(String[] args) {
+
+			Class o1 = new Class();
+			System.out.println(o1);
+
+			// static method with class name
+			Class.display();
+
+			Class o2 = new Class();
+			System.out.println(o2);
+			Class.display();
+			Class o3 = new Class();
+			System.out.println(o3);
+			Class.display();
+
+		}
+
+	}
+
+
