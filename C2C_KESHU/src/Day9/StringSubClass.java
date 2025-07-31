72eef598e9f2a2e8de782b9dc8c3366a60491397
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-07-31 10:10:51 +0530

    Core Java

---
+++ C2C_KESHU/src/Day9/StringSubClass.java
@@ -0,0 +1,14 @@
+package Day9;
+
+public class StringSubClass {
+    public static void main(String[] args) {
+        String name = "Keshu";
+        System.out.println(name);
+    }
+}
+
+
+
+
+
+
