e0f4ceef9a1b25a7673d6c590452b7b6be97c850
Sowmiya <Sowmiya@DESKTOP-86E1PE4>
2025-08-06 19:48:35 +0530

    core java

---
+++ C2C_KESHU/src/Day15/generics/GenericClass.java
@@ -0,0 +1,20 @@
+package Day15.generics;
+
+
+	//Generic Class
+	class GenericClass<T>
+	{
+		T data;
+		void set(T data)
+		{
+			this.data=data;
+		}
+		
+		T get()
+		{
+			return data;
+		}
+	}
+
+
+
---
+++ C2C_KESHU/src/Day15/generics/MinMax.java
@@ -0,0 +1,13 @@
+package Day15.generics;
+
+
+	//Generic Interface
+	public interface MinMax<T extends Comparable<T>> {
+
+		T min();
+
+		T max();
+	}
+
+
+
